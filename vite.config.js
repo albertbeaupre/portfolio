5707268bbdeac3de666a830d4d31382fcae86345
albert <you@example.com>
2025-01-27 12:04:31 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio/', // Ensure this matches your repository name exactly
+  base: '/portfolio/', // Ensure this matches your repository name exactly
   build: {
     outDir: 'build', // Change the output directory to 'build'
   },
