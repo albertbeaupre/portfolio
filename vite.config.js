a636ce6ca028989cff0c6a41cd6215aaa20c1a32
albert <you@example.com>
2025-01-27 11:41:09 -0600

    Initial Upload

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/Portfolio/', // Ensure this matches your repository name exactly
-  css: {
-    modules: {
-      localsConvention: 'camelCase',
-    },
+  build: {
+    outDir: 'build', // Change the output directory to 'build'
   },
 });
