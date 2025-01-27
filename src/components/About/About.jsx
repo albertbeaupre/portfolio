2ebb9c41dd84d5b554e95ba2340dde7184fd7e4e
albertbeaupre <albert-beaupre@hotmail.com>
2025-01-27 14:25:58 -0600

    Update About.jsx

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,7 +12,7 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
               <p>
-                Software Developer and Army Veteran with a strong foundation in programming cultivated from early exposure to the programming community at the age of 13 beginning with reverse engineering of obfuscated code. Collaborated with multiple teams to contribute to the development and deployment of software servers, gaining valuable insights into programming, server infrastructure, and network communication. Transitioned into game development, acquiring extensive knowledge of client/server architecture and network communication protocols. Adept at authoring code derived from designing, building, and maintaining software and content creation tools.
+                Software Developer and Army Veteran with a strong foundation in programming cultivated from early exposure to the programming community at the age of 13 beginning with reverse engineering obfuscated code. Collaborated with multiple teams to contribute to the development and deployment of software servers, gaining valuable insights into programming, server infrastructure, and network communication. Transitioned into game development, acquiring extensive knowledge of client/server architecture and network communication protocols. I’m all about writing clean, efficient code and building software that’s secure, thread-safe, and optimized. Whether it’s improving performance or ensuring stability, I genuinely care about getting it right. Programming isn’t just something I do, it’s something I love, and I’m always working to get better at it. .
               </p>
             </div>
           </li>
