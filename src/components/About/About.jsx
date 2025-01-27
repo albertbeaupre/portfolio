e1fc7a92f42968f4d41851e84458a0b4e1ff9550
albert <you@example.com>
2025-01-27 13:02:39 -0600

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,13 +12,7 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
               <p>
-                I began programming in Java at the age of 13, which ignited my passion for software development. 
-                Over time, I broadened my expertise by learning additional programming languages. 
-                This early foundation allowed me to delve into the inner workings of computer components, 
-                giving me a comprehensive understanding of how software interacts with hardware. 
-                Throughout time, I have had the opportunity to collaborate on diverse projects, 
-                gaining valuable experience in team environments while working with databases, networking, security, and software design. 
-                Today, I possess a strong foundation in Software Engineering and remain dedicated to continuously expanding and refining my technical skills.
+              Software Developer and Army Veteran with a strong foundation in programming cultivated from early exposure to the programming community at the age of 13 beginning with reverse engineering of obfuscated code. Collaborated with multiple teams to contribute to the development and deployment of software servers, gaining valuable insights into programming, server infrastructure, and network communication. Transitioned into game development, acquiring extensive knowledge of client/server architecture and network communication protocols. Adept at authoring code derived from designing, building, and maintaining software and content creation tools.
               </p>
             </div>
           </li>
