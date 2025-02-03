edb23f0b89cebbfdd3250d31623940980b967ea4
albertbeaupre <albert-beaupre@hotmail.com>
2025-02-02 19:48:27 -0600

    update

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,13 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/', 
+  base: '/portfolio/',
   plugins: [react()],
   build: {
     outDir: 'dist', // Change 'dist' to 'build'
+    manifest: true, // This will create a manifest file that you can use to customize the HTML output
+    rollupOptions: {
+      input: '/index.html', // Ensure that it points to your actual HTML file
+    },
   },
 });
