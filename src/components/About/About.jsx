c33b9b068776e865217fdb6dd0176bd052b43dce
albertbeaupre <albert-beaupre@hotmail.com>
2025-02-03 07:38:12 -0600

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -12,12 +12,12 @@ export const About = () => {
           <li className={styles.aboutItem}>
             <div className={styles.aboutItemText}>
               <p>
-                I’ve been passionate about programming since I was 13. I started out reverse-engineering obfuscated code. It was tough—some systems are packed with complex networks, caches, and algorithms—but figuring them out taught me how these systems work and how to build them from scratch. It was a challenge I loved, and it’s what pushed me to dive deeper into programming.
+                I’ve been passionate about programming since I was 13. I started out reverse-engineering obfuscated code. Most of the systems were packed with complex networks, caches, and algorithms—but figuring them out taught me how these systems work and how to build them from scratch. It was a challenge I loved, and it’s what pushed me to dive deeper into programming.
               </p>
               <br />
               <br />
               <p>
-                Now, I’m all about writing clean, efficient code and building software that’s secure, thread-safe, and optimized. Whether it’s improving performance or ensuring stability, I genuinely care about getting it right. Programming isn’t just something I do, it’s something I love, and I’m always working to get better at it. I’m ready to contribute however I can, even if it means starting at the bottom. I’m confident my passion and experience will make a difference.
+                Today, I’m all about writing clean, efficient code and building software that’s secure, thread-safe, and optimized. Whether it’s improving performance or ensuring stability, I genuinely care about getting it right. Programming isn’t just something I do, it’s something I love, and I’m always working to get better at it. I’m ready to contribute however I can, even if it means starting at the bottom.
 
               </p>
             </div>
