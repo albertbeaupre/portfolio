5a3ede0a4b59064ba9dbcd47a1a3fa29aeba68b3
albertbeaupre <albert-beaupre@hotmail.com>
2025-02-18 11:19:37 -0600

    update

--- src/components/Albert/Albert.jsx
+++ src/components/Albert/Albert.jsx
@@ -4,16 +4,15 @@ import styles from "./Albert.module.css";
 import { getImageUrl } from "../../utils";
 
 export const Hero = () => {
+
   return (
     <section className={styles.container}>
       <div className={styles.content}>
-        <h1 className={styles.title}>
-          Albert Beaupre
-        </h1>
-
-        <h1 className={styles.description}>
-          Software Engineer
-        </h1>
+        <h1 className={styles.title}>Albert Beaupre</h1>
+        <h1 className={styles.description}>Software Engineer</h1>
+        <a href="files/resume.docx" download="Resume for Albert Beaupre" className={styles.resumeButton}>
+          Download Resume
+        </a>
       </div>
       <img
         src={getImageUrl("images/albert.png")}
